f0b20bb4a9f76b3bf5683577089c4082dc0e2c22
Travis PENG <houdou.pt@gmail.com>
2019-01-26 08:41:51 +0800

    :building_construction: UI Framework

---
+++ Assets/Scripts/Controllers/CardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour {
+	public event Action OnClick;
+	
+	void Start() { }
+
+	void Update() { }
+
+	public void HandleClick() {
+		OnClick?.Invoke();
+	}
+}
---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+    #region Singleton
+
+    private static GameMaster instance;
+
+    private static object _lock = new object();
+
+    public static GameMaster Instance {
+        get {
+            if (applicationIsQuitting) {
+                Debug.LogWarning("[Singleton] Instance '" + typeof(GameMaster) +
+                                 "' already destroyed on application quit." +
+                                 " Won't create again - returning null.");
+                return null;
+            }
+
+            lock (_lock) {
+                if (instance == null) {
+                    instance = (GameMaster) FindObjectOfType(typeof(GameMaster));
+
+                    if (FindObjectsOfType(typeof(GameMaster)).Length > 1) {
+                        Debug.LogError("[Singleton] Something went really wrong " +
+                                       " - there should never be more than 1 singleton!" +
+                                       " Reopening the scene might fix it.");
+                        return instance;
+                    }
+
+                    if (instance == null) {
+                        GameObject singleton = new GameObject();
+                        instance = singleton.AddComponent<GameMaster>();
+                        singleton.name = "(singleton) " + typeof(GameMaster);
+
+                        DontDestroyOnLoad(singleton);
+
+                        Debug.Log("[Singleton] An instance of " + typeof(GameMaster) +
+                                  " is needed in the scene, so '" + singleton +
+                                  "' was created with DontDestroyOnLoad.");
+                    }
+                    else {
+                        Debug.Log("[Singleton] Using instance already created: " +
+                                  instance.gameObject.name);
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    private static bool applicationIsQuitting;
+
+    /// <summary>
+    /// When Unity quits, it destroys objects in a random order.
+    /// In principle, a Singleton is only destroyed when application quits.
+    /// If any script calls Instance after it have been destroyed, 
+    ///   it will create a buggy ghost object that will stay on the Editor scene
+    ///   even after stopping playing the Application. Really bad!
+    /// So, this was made to be sure we're not creating that buggy ghost object.
+    /// </summary>
+    public void OnDestroy() {
+        applicationIsQuitting = true;
+    }
+
+    #endregion
+}
