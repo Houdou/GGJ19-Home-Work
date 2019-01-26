d50386053dd47e06f6148eb8efb143118961da4e
Travis PENG <houdou.pt@gmail.com>
2019-01-27 01:33:54 +0800

    :bug: Clean up in todo expiry

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -193,6 +193,10 @@ public class EventManager : MonoBehaviour {
             foreach (var ev in data.FailedEvent) {
                 ProcessEvent(ev);
             }
+            
+            if (_listTodo.Contains(todo)) {
+                _listTodo.Remove(todo);
+            }
         };
 
         var cardController = DrawTodo(data);
